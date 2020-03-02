59c47dbc9294e590d0d3a4962683eed63ecfcaf4
Meghan Flynn <mflynn333@gmail.com>
2020-03-02 16:32:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,15 @@ class Student < InteractiveRecord
   end
   	  
   def self.column_names
-  	
+    DB[:conn].results_as_hash = true	 	  
+    
+    sql = "PRAGMA table_info('#{table_name}')"	 	  
+    
+    table_info = DB[:conn].execute(sql)	  
+    column_names = []
+	  
+	  table_info.each do |column|	    
+	    column_names << column["name"]
   end
   	  
 	  self.column_names.each do |col_name|

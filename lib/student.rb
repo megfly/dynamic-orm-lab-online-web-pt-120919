b2f7e612060113c510ba1a37c7ede8a81d90062e
Meghan Flynn <mflynn333@gmail.com>
2020-03-02 16:31:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,18 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
   
+  def self.table_name
+  	    #table_name code
+  end
+  	  
+  	  def self.column_names
+  	  #column_names code
+  	  end
+  	  
+	  self.column_names.each do |col_name|
+	    attr_accessor col_name.to_sym
+	  end
+	end
 
 
 end

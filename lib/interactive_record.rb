5028357360a5e3f198724fac6abd2c94d968fb83
Meghan Flynn <mflynn333@gmail.com>
2020-03-02 16:41:05 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -43,13 +43,11 @@ class InteractiveRecord
 	  values.join(", ")
 	end
 	
-	def save
-	  DB[:conn].execute("INSERT INTO #{table_name_for_insert} 
-	  (#{col_names_for_insert}) VALUES (?)", [values_for_insert])
-	 
-	  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM 
-	  #{table_name_for_insert}")[0][0]
-	end
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
 
 
 end

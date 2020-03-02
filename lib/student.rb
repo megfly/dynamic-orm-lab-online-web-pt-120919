6754b993756a1c4ea6a49466e48a17f4133fa27e
Meghan Flynn <mflynn333@gmail.com>
2020-03-02 16:34:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,28 +4,7 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
   
-  def self.table_name
-    self.to_s.downcase.pluralize
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
   end
-  	  
-  def self.column_names
-    DB[:conn].results_as_hash = true	 	  
-    
-    sql = "PRAGMA table_info('#{table_name}')"	 	  
-    
-    table_info = DB[:conn].execute(sql)	  
-    column_names = []
-	  
-	  table_info.each do |column|	    
-	    column_names << column["name"]
-    end
-  column_names.compact
-	end
-  	  
-	  self.column_names.each do |col_name|
-	    attr_accessor col_name.to_sym
-	  end
-	end
-
-
 end
